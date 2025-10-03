138580ec43045f94a4a1aea79a204abbfa24ef04
denisen87 <denisen87@hotmail.com>
2025-10-03 17:48:38 +0200

    Update Tabeller.java

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -2,87 +2,72 @@ package no.hvl.dat100.tabeller;
 
 public class Tabeller {
 
-    // a)
+    // a) Skriv ut tabellen
     public static void skrivUt(int[] tabell) {
+        System.out.print("[");
         for (int i = 0; i < tabell.length; i++) {
             System.out.print(tabell[i]);
-            if (i < tabell.length - 1) {
-                System.out.print(", ");
-            }
+            if (i < tabell.length - 1) System.out.print(",");
         }
-        System.out.println();
+        System.out.println("]");
     }
 
-    // b)
+    // b) Konverter tabellen til streng
     public static String tilStreng(int[] tabell) {
-        String s = "[";
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
         for (int i = 0; i < tabell.length; i++) {
-            s += tabell[i];
-            if (i < tabell.length - 1) {
-                s += ",";
-            }
+            sb.append(tabell[i]);
+            if (i < tabell.length - 1) sb.append(",");
         }
-        s += "]";
-        return s;
+        sb.append("]");
+        return sb.toString();
     }
 
-    // c)
+    // c) Summer tabellen
     public static int summer(int[] tabell) {
         int sum = 0;
-        for (int tall : tabell) {
-            sum += tall;
-        }
+        for (int t : tabell) sum += t;
         return sum;
     }
 
-	// d)
+    // d) Sjekk om tallet finnes i tabellen
     public static boolean finnesTall(int[] tabell, int tall) {
-        for (int t : tabell) {
-            if (t == tall) {
-                return true;
-            }
-        }
+        for (int t : tabell) if (t == tall) return true;
         return false;
     }
 
-	// e)
+    // e) Finn posisjonen til et tall
     public static int posisjonTall(int[] tabell, int tall) {
         for (int i = 0; i < tabell.length; i++) {
-            if (tabell[i] == tall) {
-                return i;
-            }
+            if (tabell[i] == tall) return i;
         }
         return -1;
     }
 
-	// f)
+    // f) Reverser tabellen
     public static int[] reverser(int[] tabell) {
-        int[] nyTabell = new int[tabell.length];
+        int[] ny = new int[tabell.length];
         for (int i = 0; i < tabell.length; i++) {
-            nyTabell[i] = tabell[tabell.length - 1 - i];
+            ny[i] = tabell[tabell.length - 1 - i];
         }
-        return nyTabell;
+        return ny;
     }
 
-	// g)
+    // g) Sjekk om tabellen er sortert stigende
     public static boolean erSortert(int[] tabell) {
         for (int i = 0; i < tabell.length - 1; i++) {
-            if (tabell[i] > tabell[i + 1]) {
-                return false;
-            }
+            if (tabell[i] > tabell[i + 1]) return false;
         }
         return true;
     }
-	// h)
-    public static int[] settSammen(int[] tabell1, int[] tabell2) {
-        int[] nyTabell = new int[tabell1.length + tabell2.length];
-        for (int i = 0; i < tabell1.length; i++) {
-            nyTabell[i] = tabell1[i];
-        }
-        for (int i = 0; i < tabell2.length; i++) {
-            nyTabell[tabell1.length + i] = tabell2[i];
-        }
-        return nyTabell;
+
+    // h) Sett sammen to tabeller
+    public static int[] settSammen(int[] tab1, int[] tab2) {
+        int[] ny = new int[tab1.length + tab2.length];
+        for (int i = 0; i < tab1.length; i++) ny[i] = tab1[i];
+        for (int i = 0; i < tab2.length; i++) ny[tab1.length + i] = tab2[i];
+        return ny;
     }
 }
 
